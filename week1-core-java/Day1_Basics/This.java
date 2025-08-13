ddee5e9d4a8eea32afa6b92df61b72a68dbd8b9b
Tushant27 <tualoney27@gmail.com>
2025-08-13 10:44:29 +0530

    The program is about .this keyword

---
+++ week1-core-java/Day1_Basics/This.java
@@ -0,0 +1,25 @@
+class Q
+{
+	int age;
+	String name;
+	Q(int age,String name)
+	{
+		this.age=age;
+		this.name=name;
+	}
+	public void print()
+	{
+		System.out.println(age+   +name);
+	}
+}
+
+
+public class This {
+
+	public static void main(String[] args) {
+		 TODO Auto-generated method stub
+        Q z=new Q(27,yoyo);
+        z.print();
+	}
+
+}
