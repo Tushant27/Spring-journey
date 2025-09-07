c6434b41463df152dea9af22b64a13b9aee12310
Tushant27 <tualoney27@gmail.com>
2025-09-07 22:41:20 +0530

    Constructor class

---
+++ week1-core-java/Day1_Basics/Constructor.java
@@ -0,0 +1,17 @@
+public class Main {
+  
+  Main() {
+    System.out.println("Hello, World!");
+  }
+
+  public static void main(String[] args) {
+    System.out.println("The main() method.");
+
+    Main obj = new Main();
+  }
+}
+
+
+//OP:
+//The main() method
+//Hello, World!
